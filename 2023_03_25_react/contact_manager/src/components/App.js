d0800cd238e2924ae26f53d2e2985fbb3da74e60
Shashisuji <sk.ss1012@gmail.com>
2023-08-02 00:07:53 +0530

    call , apply, bind and react basic

---
+++ 2023_03_25_react/contact_manager/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+// import AddContact from "./AddContact";
+// import ContactCard from "./ContactCard";
+
+
+function App() {
+  return (
+  //  <h1>Hello World!</h1>
+  <Header></Header>
+  // <AddContact></AddContact>
+  // <ContactCard></ContactCard>
+  );
+}
+
+export default App;
